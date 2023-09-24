6ed00d96157f26e6f8fcdc19817e1e83b53abe3c
Ali-pew <isaali032@gmail.com>
2023-09-24 22:04:40 +0600

    New print

--- bin/git_traib_2.dart
+++ bin/git_traib_2.dart
@@ -2,4 +2,5 @@ import 'package:git_traib_2/git_traib_2.dart' as git_traib_2;
 
 void main(List<String> arguments) {
   print('Hello world: ${git_traib_2.calculate()}!');
+  print("Hello git ${git_traib_2.calculate()}");
 }
